25d359c1f3a4de00b8eccf32c7ffdcd6363de794
rdmh12 <276881386+rdmh12@users.noreply.github.com>
2026-06-04 14:20:35 +0200

    Display Pokémon cards in random order

--- src/App.jsx
+++ src/App.jsx
@@ -3,102 +3,111 @@ import PokemonCard from "./PokemonCard.jsx";
 
 let initialized = false;
 
-const testPokemonData = [
-	{
-		"id": 137,
+const testPokemonData = {
+	[137]: {
 		"name": "Porygon",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/137.png"
 	},
-	{
-		"id": 126,
+	[126]: {
 		"name": "Magmar",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/126.png"
 	},
-	{
-		"id": 156,
+	[156]: {
 		"name": "Quilava",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/156.png"
 	},
-	{
-		"id": 236,
+	[236]: {
 		"name": "Tyrogue",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/236.png"
 	},
-	{
-		"id": 86,
+	[86]: {
 		"name": "Seel",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/86.png"
 	},
-	{
-		"id": 114,
+	[114]: {
 		"name": "Tangela",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/114.png"
 	},
-	{
-		"id": 212,
+	[212]: {
 		"name": "Scizor",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/212.png"
 	},
-	{
-		"id": 125,
+	[125]: {
 		"name": "Electabuzz",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/125.png"
 	},
-	{
-		"id": 43,
+	[43]: {
 		"name": "Oddish",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/43.png"
 	},
-	{
-		"id": 141,
+	[141]: {
 		"name": "Kabutops",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/141.png"
 	},
-	{
-		"id": 177,
+	[177]: {
 		"name": "Natu",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/177.png"
 	},
-	{
-		"id": 37,
+	[37]: {
 		"name": "Vulpix",
 		"sprite": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/37.png"
 	}
-];
+};
+
+function shufflePokemons(pokemonData) {
+	const order = Array.from(Object.keys(pokemonData));
+
+	for (let index = order.length - 1; index > 0; index--) {
+		const randomIndex = Math.floor(Math.random() * (index + 1));
+
+		[order[index], order[randomIndex]] = [order[randomIndex], order[index]];
+	}
+
+	return order;
+}
 
 async function fetchPokemonData() {
 	const pokemonCount = 12;
 	const maxPokemonId = 251;
-	const pokemonData = new Array(pokemonCount);
+	const pokemonData = {};
 
 	for (let index = 0; index < pokemonCount; index++) {
 		const id = Math.floor(Math.random() * maxPokemonId) + 1;
 		const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
 		const result = await response.json();
 
-		pokemonData[index] = {
-			id: result.id,
+		pokemonData[result.id] = {
 			name: result.name.charAt(0).toUpperCase() + result.name.slice(1),
 			sprite: result.sprites.other["official-artwork"].front_default,
 		};
 	}
 
-	return pokemonData;
+	const pokemonOrder = shufflePokemons(pokemonData);
+
+	return [pokemonData, pokemonOrder];
 }
 
 export default function App() {
 	const [pokemonData, setPokemonData] = useState(testPokemonData);
+	const [pokemonOrder, setPokemonOrder] = useState(shufflePokemons(testPokemonData));
 
+	// const [pokemonData, setPokemonData] = useState({});
+	// const [pokemonOrder, setPokemonOrder] = useState(shufflePokemons([]));
+	//
 	// useEffect(() => {
 	// 	if (!initialized) {
 	// 		initialized = true;
-	// 		fetchPokemonData().then((pokemonData) => setPokemonData(pokemonData));
+	// 		fetchPokemonData().then(([pokemonData, pokemonOrder]) => {
+	// 			setPokemonData(pokemonData);
+	// 			setPokemonOrder(pokemonOrder);
+	// 		});
 	// 	}
 	// }, []);
 
-	const pokemonCards = pokemonData.map((pokemon) => {
+	const pokemonCards = pokemonOrder.map((pokemonId) => {
+		const pokemon = pokemonData[pokemonId];
 		return (
-			<PokemonCard key={pokemon.id} pokemon={pokemon} />
+			<PokemonCard key={pokemonId} pokemon={pokemon} />
 		);
 	});
 
--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,6 +1,6 @@
 export default function PokemonCard({ pokemon }) {
 	return (
-		<div key={pokemon.id} className="pokemon-card">
+		<div className="pokemon-card">
 			<img src={pokemon.sprite} alt={pokemon.name} className="pokemon-card-sprite" />
 			<div className="pokemon-card-name">{pokemon.name}</div>
 		</div>
