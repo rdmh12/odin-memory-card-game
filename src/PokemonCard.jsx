594afed4ef835bb3d4335aab5234dd086e133819
rdmh12 <276881386+rdmh12@users.noreply.github.com>
2026-06-05 13:52:08 +0200

    Add styling

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,6 +1,6 @@
 export default function PokemonCard({ pokemon, onPicked }) {
 	return (
-		<div className="pokemon-card" onClick={onPicked}>
+		<div className="pokemon-card" onClick={onPicked} tabIndex="0">
 			<img src={pokemon.sprite} alt={pokemon.name} className="pokemon-card-sprite" />
 			<div className="pokemon-card-name">{pokemon.name}</div>
 		</div>
